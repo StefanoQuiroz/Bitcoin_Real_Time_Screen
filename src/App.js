9d876128fbc33eecde6be9d05837bcbde8fb00d3
Stefano <piero.quiroz93@gmail.com>
2021-08-29 23:31:22 -0500

    Intentos de WebSocket

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import StockChart from './components/StockChart';
 
 
 function App() {
-  let webSocket = new WebSocket(`wss://stream.binance.com:9443/ws/dogeusdt@trade`);
+  //let webSocket = new WebSocket(`wss://stream.binance.com:9443/ws/dogeusdt@trade`);
+  //let webSocket = new WebSocket(`wss://ws.coincap.io/prices?assets`);
+  //let webSocket = new WebSocket(`wss://ws.coincap.io/trades/binance`);
   const [price, setPrice] = useState("");
   webSocket.onmessage = (event) => {
-    const stock = JSON.parse(event.data);
-    const priceFloat = parseFloat(stock.p).toFixed(4);
-    setPrice(priceFloat);
+    const stock = (event.data);
+    //const priceFloat = parseFloat(stock.p).toFixed(4);
+    //setPrice(priceFloat);
+    setPrice(stock);
   }
 
-  //console.log(price);
+  console.log("Stocks", price);
   
 
   return (
     <div className="App">
-      <h1 style={{display: "flex", justifyContent: "center", marginTop: "5rem"}}> DogeCoin : {price}</h1>
+      {/* <h1 style={{display: "flex", justifyContent: "center", marginTop: "5rem"}}> DogeCoin : {price}</h1> */}
     </div>
   );
 }
