790396540a4df14660cc06d628894222ee0eac51
Stefano <piero.quiroz93@gmail.com>
2021-08-29 19:56:56 -0500

    Prueba Real Data time with WebSockets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import './App.scss';
+import StockChart from './components/StockChart';
+
 
 function App() {
+  let webSocket = new WebSocket(`wss://stream.binance.com:9443/ws/dogeusdt@trade`);
+  const [price, setPrice] = useState("");
+  webSocket.onmessage = (event) => {
+    const stock = JSON.parse(event.data);
+    const priceFloat = parseFloat(stock.p).toFixed(4);
+    setPrice(priceFloat);
+  }
+
+  //console.log(price);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{display: "flex", justifyContent: "center", marginTop: "5rem"}}> DogeCoin : {price}</h1>
     </div>
   );
 }
